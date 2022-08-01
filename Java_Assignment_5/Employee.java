9b94eb146e9e79a2dc1a8b44dc090d02f6db90fc
CLSNeko <cl_staples@protonmail.ch>
2022-07-31 23:25:15 -0400

    Removed extra class files

---
+++ Java_Assignment_5/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+		int id;
+		String name;
+		int age;
+		String gender;
+		String dept;
+		int yearOfJoining;
+		int salary;
+
+		public static void main(String[] args){
+		}
+}
